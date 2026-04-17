058d31b7f93e9417c265a090f77017a4c9ad60fb
adminPEALabs <patricio.pedersoli@peaenergia.com>
2026-04-17 19:19:12 -0300

    proyecto: integracion de google-services.json
    Conectamos proyecto con Firebase OK

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,6 +2,7 @@ plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
     alias(libs.plugins.kotlin.compose)
+    id("com.google.gms.google-services")
 }
 
 android {
@@ -60,4 +61,7 @@ dependencies {
     androidTestImplementation(libs.androidx.ui.test.junit4)
     debugImplementation(libs.androidx.ui.tooling)
     debugImplementation(libs.androidx.ui.test.manifest)
+
+    implementation(platform("com.google.firebase:firebase-bom:34.12.0"))
+
 }
