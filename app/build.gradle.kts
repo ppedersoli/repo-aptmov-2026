f400964629540a21a5d5c11c61fa4c33d5cff171
adminPEALabs <patricio.pedersoli@peaenergia.com>
2026-04-24 19:18:47 -0300

    config: update sdk target
    Actualizamos SDK para version Panda de ASTUDIO

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -7,12 +7,12 @@ plugins {
 
 android {
     namespace = "com.epm.apmovil2026"
-    compileSdk = 35
+    compileSdk = 36
 
     defaultConfig {
         applicationId = "com.epm.apmovil2026"
         minSdk = 24
-        targetSdk = 35
+        targetSdk = 36
         versionCode = 1
         versionName = "1.0"
 
